4448aac7cdc8bc44b58da7f249c261c1b16e8684
russianpanda <vladgavinas@gmail.com>
2021-06-19 19:18:13 +0100

    Commit initial project files

---
+++ README.md
@@ -0,0 +1,25 @@
+# PhotoSorter
+PhotoSorter is a simple Python script that sorts the picture and video contents of a directory into appropriate sub-directories based on file extensions, such as ```.arw```, ```.mov```, ```.jpg```. PhotoSorter supports most commonly used photo and video file extensions.
+
+The inspiration behind this script came from having tons of unsorted pictures and videos that I took with my camera and wanting to automate the process.
+
+## Installation
+No installation of external dependencies required, script uses Python3.8 and built-in libraries.
+
+## Usage
+Currently, the script works by placing it into a main directory with all files to be sorted.
+
+```bash
+python photo_sort.py
+```
+
+## Future Work
+* Add command-line arguments to accept custom directories (i.e not the one the script is located in)
+* Add an ability for the script to scan sub-directories for files to sort
+* Add custom sorting filters, e.g. date/name
+
+## Contributing
+Contributions, issues, and feature requests are welcome!
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ photo_sort.py
@@ -0,0 +1,55 @@
+import os 
+
+def main():
+    # Populate with most common raw photo file extensions
+    # Source: https://en.wikipedia.org/wiki/Raw_image_format
+    RAW_IMAGE_FORMATS = [
+        '3fr', 'ari', 'arw', 'bay', 'braw', 'crw', 'cr2', 'cr3', 'cap', 'data', 'dcs', 'dcr', 'dng', 'drf',
+        'eip', 'erf', 'fff', 'gpr', 'iiq', 'k25', 'kdc', 'mdc', 'mef', 'mos', 'mrw', 'nef', 'nrw', 'obm',
+        'orf', 'pef', 'ptx', 'pxn', 'r3d', 'raf', 'raw', 'rw1', 'rw2', 'rwz', 'sr2', 'srf', 'srw', 'tif', 'x3f'
+    ]
+
+    # Populate with most common video file extensions
+    # Source: https://videoconverter.wondershare.com/dv/full-guide-to-camera-video-file-formats.html
+    VIDEO_FORMATS = [
+        'mp4', 'm4p', 'm4b', 'm4r', 'm4v', 'm4a', 'divx', 'evo', 'f4v', 'flv',
+        'avi', 'qt', 'mxf', 'mov', 'mts', 'm2ts', 'mpeg', 'vob', 'ifo'
+    ]
+
+    # Iterate through each file in the directory
+    for file in os.listdir(os.curdir):
+        # Extract the file extension from the full path, without the '.'
+        file_extension = os.path.splitext(file)[1][1:].lower()
+
+        # Create an empty directory for future edits
+        createDirectory('Edits')
+
+        # If directory has RAW images, create a folder for them
+        if file_extension in RAW_IMAGE_FORMATS:
+            populateDirectory(file, 'RAW')
+
+        # If directory has videos, create a folder for them
+        if file_extension in VIDEO_FORMATS:
+            populateDirectory(file, 'Video')
+
+        # If directory has JPEG images, create a folder for them too
+        if file_extension == 'jpeg' or file_extension.lower() == 'jpg':
+            populateDirectory(file, 'JPEG')
+        
+
+def createDirectory(dir):
+    # Check if directory with name dir exists, if not, create it 
+    if not os.path.isdir(dir):
+        os.mkdir(dir)
+
+def moveFile(dir, file):
+    # Move file from its current location into dir
+    os.replace(file, f'{dir}/{file}')
+
+def populateDirectory(file, dir):
+    # Create the directory and move files
+    createDirectory(dir)
+    moveFile(dir, file)
+
+if __name__ == '__main__':
+    main()
